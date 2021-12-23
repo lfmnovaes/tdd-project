84108d4d1e4c3e5245b2a5bceb86595f29a19514
mike2611 <mpuentes2611@gmail.com>
2021-12-23 14:03:26 -0600

    Add more tests

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,7 +4,19 @@ describe Solver do
   solver = Solver.new
   context 'Test method factorial' do
     it 'Returns factorial of a number' do
+      expect(solver.factorial(5)).to be_an_instance_of(Integer)
+    end
+    it 'Returns factorial of a number' do
       expect(solver.factorial(5)).to eq(120)
+    end
+    it 'Returns factorial of a number' do
+      expect(solver.factorial(0)).to eq(1)
+    end
+    it 'Returns factorial of a number' do
+      expect(solver.factorial(1)).to eq(1)
+    end
+    it 'Returns factorial of a number' do
+      expect(solver.factorial(2)).to eq(2)
     end
   end
 end
